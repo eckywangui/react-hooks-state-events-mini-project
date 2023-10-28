5054ce61a2997ab73bbb270926582357a1d55d3c
Eckra Wangui <+>
2023-10-28 21:23:29 +0300

    completed assignment

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -11,7 +11,7 @@ function TaskList({tasks}) {
 }
   return (
     <div className="tasks">
-      {/* display a list of tasks using Task component */}
+     
       {tasks.map((task) => (
         <Task
         key={task.id}
