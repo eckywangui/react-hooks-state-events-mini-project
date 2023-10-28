859b44a08ee7cb2b3c540339b79576a7e4decbbc
Eckra Wangui <+>
2023-10-28 21:22:33 +0300

    completed assignment

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [newTask, setNewTask] = useState({
+    text: "",
+    category: categories[0], 
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setNewTask((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  
+    onTaskFormSubmit(newTask);
+    
+    setNewTask({
+      text: "",
+      category: categories[0],
+    });
+  };
+
   return (
-    <form className="new-task-form">
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input
+          type="text"
+          name="text"
+          value={newTask.text}
+          onChange={handleChange}
+        />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select
+          name="category"
+          value={newTask.category}
+          onChange={handleChange}
+        >
+          {categories.map((category) => (
+            <option key={category} value={category}>
+              {category}
+            </option>
+          ))}
         </select>
       </label>
       <input type="submit" value="Add task" />
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
+import Task from "./Task";
 
-function TaskList() {
+function TaskList({tasks}) {
+  const[taskList, setTaskList]=useState(tasks);
+  const handleDeleteTask= (taskId)=>{
+  
+  const updatedTaskList= taskList.filter((task) => task.id !== taskId);
+  
+  setTaskList(updatedTaskList);
+}
   return (
     <div className="tasks">
       {/* display a list of tasks using Task component */}
+      {tasks.map((task) => (
+        <Task
+        key={task.id}
+        category={task.category}
+        text={task.text}
+        onDelete={()=> handleDeleteTask(task.id)}
+        />
+      ))}
     </div>
   );
 }
